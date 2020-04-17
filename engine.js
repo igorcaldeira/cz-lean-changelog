d5f0a7ee20a03af6cbbe0ebe5fe22c1ea3e98e0f
Igor Caldeira <igorcaldeira@ioasys.com.br>
2020-04-17 16:32:36 -0300

    simple version

--- engine.js
+++ engine.js
@@ -6,23 +6,23 @@ var longest = require('longest');
 var rightPad = require('right-pad');
 var chalk = require('chalk');
 
-var filter = function(array) {
-  return array.filter(function(x) {
+var filter = function (array) {
+  return array.filter(function (x) {
     return x;
   });
 };
 
-var headerLength = function(answers) {
+var headerLength = function (answers) {
   return (
     answers.type.length + 2 + (answers.scope ? answers.scope.length + 2 : 0)
   );
 };
 
-var maxSummaryLength = function(options, answers) {
+var maxSummaryLength = function (options, answers) {
   return options.maxHeaderWidth - headerLength(answers);
 };
 
-var filterSubject = function(subject) {
+var filterSubject = function (subject) {
   subject = subject.trim();
   if (subject.charAt(0).toLowerCase() !== subject.charAt(0)) {
     subject =
@@ -37,14 +37,14 @@ var filterSubject = function(subject) {
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
-module.exports = function(options) {
+module.exports = function (options) {
   var types = options.types;
 
   var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function(type, key) {
+  var choices = map(types, function (type, key) {
     return {
       name: rightPad(key + ':', length) + ' ' + type.description,
-      value: key
+      value: key,
     };
   });
 
@@ -60,7 +60,7 @@ module.exports = function(options) {
     //
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
-    prompter: function(cz, commit) {
+    prompter: function (cz, commit) {
       // Let's ask some questions of the user
       // so that we can populate our commit
       // template.
@@ -74,24 +74,12 @@ module.exports = function(options) {
           name: 'type',
           message: "Select the type of change that you're committing:",
           choices: choices,
-          default: options.defaultType
-        },
-        {
-          type: 'input',
-          name: 'scope',
-          message:
-            'What is the scope of this change (e.g. component or file name): (press enter to skip)',
-          default: options.defaultScope,
-          filter: function(value) {
-            return options.disableScopeLowerCase
-              ? value.trim()
-              : value.trim().toLowerCase();
-          }
+          default: options.defaultType,
         },
         {
           type: 'input',
           name: 'subject',
-          message: function(answers) {
+          message: function (answers) {
             return (
               'Write a short, imperative tense description of the change (max ' +
               maxSummaryLength(options, answers) +
@@ -99,7 +87,7 @@ module.exports = function(options) {
             );
           },
           default: options.defaultSubject,
-          validate: function(subject, answers) {
+          validate: function (subject, answers) {
             var filteredSubject = filterSubject(subject);
             return filteredSubject.length == 0
               ? 'subject is required'
@@ -111,7 +99,7 @@ module.exports = function(options) {
                 filteredSubject.length +
                 ' characters.';
           },
-          transformer: function(subject, answers) {
+          transformer: function (subject, answers) {
             var filteredSubject = filterSubject(subject);
             var color =
               filteredSubject.length <= maxSummaryLength(options, answers)
@@ -119,82 +107,31 @@ module.exports = function(options) {
                 : chalk.red;
             return color('(' + filteredSubject.length + ') ' + subject);
           },
-          filter: function(subject) {
+          filter: function (subject) {
             return filterSubject(subject);
-          }
-        },
-        {
-          type: 'input',
-          name: 'body',
-          message:
-            'Provide a longer description of the change: (press enter to skip)\n',
-          default: options.defaultBody
+          },
         },
         {
           type: 'confirm',
           name: 'isBreaking',
           message: 'Are there any breaking changes?',
-          default: false
-        },
-        {
-          type: 'input',
-          name: 'breakingBody',
-          default: '-',
-          message:
-            'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself:\n',
-          when: function(answers) {
-            return answers.isBreaking && !answers.body;
-          },
-          validate: function(breakingBody, answers) {
-            return (
-              breakingBody.trim().length > 0 ||
-              'Body is required for BREAKING CHANGE'
-            );
-          }
+          default: false,
         },
         {
           type: 'input',
           name: 'breaking',
           message: 'Describe the breaking changes:\n',
-          when: function(answers) {
+          when: function (answers) {
             return answers.isBreaking;
-          }
-        },
-
-        {
-          type: 'confirm',
-          name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
-          default: options.defaultIssues ? true : false
-        },
-        {
-          type: 'input',
-          name: 'issuesBody',
-          default: '-',
-          message:
-            'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself:\n',
-          when: function(answers) {
-            return (
-              answers.isIssueAffected && !answers.body && !answers.breakingBody
-            );
-          }
-        },
-        {
-          type: 'input',
-          name: 'issues',
-          message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
-          when: function(answers) {
-            return answers.isIssueAffected;
           },
-          default: options.defaultIssues ? options.defaultIssues : undefined
-        }
-      ]).then(function(answers) {
+        },
+      ]).then(function (answers) {
         var wrapOptions = {
           trim: true,
           cut: false,
           newline: '\n',
           indent: '',
-          width: options.maxLineWidth
+          width: options.maxLineWidth,
         };
 
         // parentheses are only needed when a scope is present
@@ -217,6 +154,6 @@ module.exports = function(options) {
 
         commit(filter([head, body, breaking, issues]).join('\n\n'));
       });
-    }
+    },
   };
 };
